0af38ca46d142f33102f26dbaff7ff98d64341da
nouun <me@nouun.dev>
2022-01-12 08:58:57 +0000

    [fix] restructure for pypi

--- setup.py
+++ setup.py
@@ -4,20 +4,25 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="lunarcrushed",
-    version="0.2.2",
+    name='lunarcrushed',
+    version='0.1.2',
+
     description="Generate an API key for LunarCRUSH.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="Nouun",
-    author_email="me@nouun.dev",
-    license="GNU-3.0-or-later",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    url="https://github.com/nouun/lunarcrushed/tree/python",
-    project_urls={
-        "Bug Tracker": "https://github.com/nouun/lunarcrushed/issues",
-    },
-    keywords="lunarcrush crypto",
-    install_requires=["requests"],
+
+    url='https://github.com/nouun/lunarcrushed',
+
+    author='nouun',
+    author_email='me@nouun.dev',
+
+    license='GNU 3.0 or later',
+
+    keywords='api lunarcrush trading crypto bitcoin altcoin',
+
+    packages=find_packages(),
+
+    install_requires=[
+        'requests',
+    ],
 )
