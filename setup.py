c7bb9797ad06b8877dce1d00686d37f30c431a78
nouun <me@nouun.dev>
2022-01-12 06:43:11 +0000

    [chore] restructure project

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="lunarcrushed",
-    version="0.1.0",
+    version="0.2.2",
     description="Generate an API key for LunarCRUSH.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -14,7 +14,10 @@ setup(
     license="GNU-3.0-or-later",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    url="https://github.com/gmyrianthous/example-publish-pypi",
+    url="https://github.com/nouun/lunarcrushed/tree/python",
+    project_urls={
+        "Bug Tracker": "https://github.com/nouun/lunarcrushed/issues",
+    },
     keywords="lunarcrush crypto",
     install_requires=["requests"],
 )
